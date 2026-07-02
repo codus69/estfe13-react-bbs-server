9b8ce7d263cd598bc5fe4cd4c47e8de490cff2ef
이채연 <196144872+codus69@users.noreply.github.com>
2026-07-02 16:12:58 +0900

    최종 비번 설정

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mysql = require('mysql2');
+const port = 3000;
+
+let corsOptions = {
+  origin: '*',
+};
+
+app.use(cors(corsOptions));
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'qwer1234!',
+  database: 'bbs',
+});
+
+db.connect();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.get('/list', (req, res) => {
+  const sqlQuery = "SELECT id, title, content, writer, DATE_FORMAT(date, '%Y-%m-%d') AS date FROM board;";
+  db.query(sqlQuery, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
